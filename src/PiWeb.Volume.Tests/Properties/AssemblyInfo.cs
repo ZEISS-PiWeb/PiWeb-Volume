91c31e7e32efb34420c84367fe01999028122c52
Daniel Flemming <daniel.flemming@zeiss.com>
2020-12-22 08:15:48 +0100

    feat: Add test project

---
+++ src/PiWeb.Volume.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿#region usings
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle( "PiWeb Volume Tests" )]
+
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyCompany( "Carl Zeiss Innovationszentrum für Messtechnik GmbH" )]
+[assembly: AssemblyProduct( "PiWeb" )]
+
+[assembly: AssemblyCopyright( "Copyright © 2020 Carl Zeiss Innovationszentrum für Messtechnik GmbH" )]
+[assembly: AssemblyTrademark( "PiWeb" )]
