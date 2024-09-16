3034ca398d5fb55ae39af530e5684c888d5df833
Jan Schneider <jan.schneider@zeiss.com>
2024-09-16 16:35:41 +0200

    task: replace copyright header

--- src/PiWeb.Volume/ReaderWriterExtensions.cs
+++ src/PiWeb.Volume/ReaderWriterExtensions.cs
@@ -1,7 +1,7 @@
 ﻿#region Copyright
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * */
-/* Carl Zeiss IMT (IZfM Dresden)                   */
+/* Carl Zeiss Industrielle Messtechnik GmbH        */
 /* Softwaresystem PiWeb                            */
 /* (c) Carl Zeiss 2019                             */
 /* * * * * * * * * * * * * * * * * * * * * * * * * */
