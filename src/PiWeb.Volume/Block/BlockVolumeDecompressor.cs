aabb720e0f387559fe925f70f44a93b95bec1ff2
Jan Schneider <jan.schneider@zeiss.com>
2024-09-22 09:58:08 +0200

    refactor: use span based data handling in decompressor

--- src/PiWeb.Volume/Block/BlockVolumeDecompressor.cs
+++ src/PiWeb.Volume/Block/BlockVolumeDecompressor.cs
@@ -31,6 +31,7 @@ namespace Zeiss.PiWeb.Volume.Block
 		private readonly ushort _SizeZ;
 		private readonly ushort _SizeY;
 		private readonly ushort _SizeX;
+		private readonly byte[][] _Result;
 
 		#endregion
 
@@ -45,6 +46,8 @@ namespace Zeiss.PiWeb.Volume.Block
 			_SizeZ = _Metadata.SizeZ;
 			_SizeY = _Metadata.SizeY;
 			_SizeX = _Metadata.SizeX;
+
+			_Result = VolumeSliceHelper.CreateSliceBuffer( _SizeX, _SizeY, _SizeZ );
 		}
 
 		#endregion
@@ -53,33 +56,36 @@ namespace Zeiss.PiWeb.Volume.Block
 
 		internal VolumeSlice[] Decompress( IProgress<VolumeSliceDefinition>? progress = null, CancellationToken ct = default )
 		{
-			if( _Volume.CompressedData[ Direction.Z ] is not {} data )
+			if( _Volume.CompressedData[ Direction.Z ] is not { } data )
 				throw new NotSupportedException( Resources.GetResource<Volume>( "CompressedDataMissing_ErrorText" ) );
 
-			var result = VolumeSliceHelper.CreateSliceBuffer( _SizeX, _SizeY, _SizeZ );
+			BlockVolumeDecoder.Decode( data, _Metadata, CopyBlockToResult, null, null, progress, ct );
 
-			BlockVolumeDecoder.Decode( data, _Metadata, ( block, index ) =>
-			{
-				for( var bz = 0; bz < BlockVolume.N; bz++ )
-				for( var by = 0; by < BlockVolume.N; by++ )
-				for( var bx = 0; bx < BlockVolume.N; bx++ )
-				{
-					var gz = index.Z * BlockVolume.N + bz;
-					var gy = index.Y * BlockVolume.N + by;
-					var gx = index.X * BlockVolume.N + bx;
-
-					if( gz >= _SizeZ || gy >= _SizeY || gx >= _SizeX )
-						continue;
-
-					result[ gz ][ gy * _SizeX + gx ] = block[ bz * BlockVolume.N2 + by * BlockVolume.N + bx ];
-				}
-			}, null, null, progress, ct );
-
-			return result
+			return _Result
 				.AsParallel()
 				.AsOrdered()
 				.Select( ( s, i ) => new VolumeSlice( new VolumeSliceDefinition( Direction.Z, (ushort)i ), s ) )
 				.ToArray();
+		}
+
+		private void CopyBlockToResult( ReadOnlySpan<byte> block, BlockIndex index )
+		{
+			var gx = index.X * BlockVolume.N;
+			var sx = Math.Min( _SizeX - index.X * BlockVolume.N, BlockVolume.N );
+			var sy = Math.Min( _SizeY - index.Y * BlockVolume.N, BlockVolume.N );
+			var sz = Math.Min( _SizeZ - index.Z * BlockVolume.N, BlockVolume.N );
+
+			for( var bz = 0; bz < sz; bz++ )
+			{
+				var gz = index.Z * BlockVolume.N + bz;
+				var sliceData = _Result[ gz ].AsSpan();
+
+				for( var by = 0; by < sy; by++ )
+				{
+					var gy = index.Y * BlockVolume.N + by;
+					block.Slice( bz * BlockVolume.N2 + by * BlockVolume.N, sx ).CopyTo( sliceData.Slice( gy * _SizeX + gx, sx ) );
+				}
+			}
 		}
 
 		#endregion
